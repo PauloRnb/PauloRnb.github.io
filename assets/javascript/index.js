783dd47441cb403b2a987c30b7f62529bf53969e
thebestchampionship <130861327+PauloRnb@users.noreply.github.com>
2024-12-17 23:24:06 -0300

    Add files via upload

---
+++ assets/javascript/index.js
@@ -0,0 +1,38 @@
+// Swiper JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// Card Menu Mobile
+const listCard = document.getElementById('list__card'),
+      closeMenu = document.getElementById('close__menu'),
+      toggleMenu = document.getElementById('toggle__menu')
+
+// Show Menu
+if (toggleMenu) {
+  toggleMenu.addEventListener('click', () => {
+    listCard.classList.add('show-menu')
+  })
+}
+
+// Hidden Menu
+if (closeMenu) {
+  closeMenu.addEventListener('click', () => {
+    listCard.classList.remove('show-menu')
+  })
+}
