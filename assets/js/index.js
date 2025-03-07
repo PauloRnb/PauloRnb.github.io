eba8f949d5776f14930720cfbc3380e6e746c304
thebestchampionship <130861327+PauloRnb@users.noreply.github.com>
2025-03-07 14:38:54 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,7 @@ var subAcess = document.getElementById('submenu-mobile'),
 
 
 // Dropdown
-if(btnSize) {
+if (btnSize) {
     btnSize.addEventListener('click', () => {
         boxDropdown.classList.add('open-dropdown')
     })
@@ -21,36 +21,54 @@ if(btnSize) {
 
 // Hidden
 
-if(closeSize) {
+if (closeSize) {
     closeSize.addEventListener('click', () => {
         boxDropdown.classList.remove('open-dropdown')
     })
 }
 
 // Menu Mobile
-if(menuMb) {
+if (menuMb) {
     menuMb.addEventListener('click', () => {
         menuCard.classList.add('open-menu')
     })
 }
 
 // Hidden
-if(closeMb) {
+if (closeMb) {
     closeMb.addEventListener('click', () => {
         menuCard.classList.remove('open-menu')
     })
 }
 
 // Sub Menu Mobile
-if(subAcess) {
+if (subAcess) {
     subAcess.addEventListener('click', () => {
         subMenu.classList.add('open-menu-mb')
     })
 }
 
 // Hiden
-if(closeAcess) {
+if (closeAcess) {
     closeAcess.addEventListener('click', () => {
         subMenu.classList.remove('open-menu-mb')
     })
 }
+
+// Swipper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
