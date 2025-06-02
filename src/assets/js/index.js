aac8f7662c562b17186a7a41c77ad51e9c576984
thebestchampionship <130861327+PauloRnb@users.noreply.github.com>
2025-06-02 14:02:37 -0300

    Add files via upload

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -243,3 +243,16 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target === modal) modal.classList.add("hidden");
     });
 });
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
